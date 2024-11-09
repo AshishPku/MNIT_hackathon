77e631d043a9f5d1cb5c1749bbd38636eded61e9
AshishPku <2022uec1698@mnit.ac.in>
2024-11-09 09:20:14 +0530

    Code for hackathon

---
+++ fronted/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./components/Header/NavBar";
+import Main from "./components/Main";
+const App = () => {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <Main></Main>
+    </div>
+  );
+};
+
+export default App;
